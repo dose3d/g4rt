e2d5e73038413ea8d85055b3e05625e2f9198a9c
Bartek Rachwal <brachwal@agh.edu.pl>
2024-02-13 11:41:19 +0100

    redesign CsvRunAnalysis structure

--- core/analysis/CsvRunAnalysis.cc
+++ core/analysis/CsvRunAnalysis.cc
@@ -1,4 +1,18 @@
 #include "CsvRunAnalysis.hh"
+#include "Services.hh"
 
 ////////////////////////////////////////////////////////////////////////////////
 ///
+CsvRunAnalysis::CsvRunAnalysis(){
+    for(const auto& scoring_type: m_scoring_types){
+        LOGSVC_INFO("Adding new map for scoring type: {}",Scoring::to_string(scoring_type));
+        m_hashed_scoring_map[scoring_type] = Service<GeoSvc>()->Patient()->GetScoringHashedMap(scoring_type);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///
+CsvRunAnalysis *CsvRunAnalysis::GetInstance() {
+    static CsvRunAnalysis instance = CsvRunAnalysis();
+    return &instance;
+}
--- core/analysis/CsvRunAnalysis.hh
+++ core/analysis/CsvRunAnalysis.hh
@@ -6,16 +6,32 @@
 #define CSV_RUN_ANALYSIS_HH
 
 #include "globals.hh"
+#include "Types.hh"
+#include "VoxelHit.hh"
 
 class CsvRunAnalysis {
     private:
-
-    public:
         ///
-        CsvRunAnalysis() = default;
+        CsvRunAnalysis();
 
         ///
         ~CsvRunAnalysis() = default;
-}
+
+        /// Delete the copy and move constructors
+        CsvRunAnalysis(const CsvRunAnalysis &) = delete;
+        CsvRunAnalysis &operator=(const CsvRunAnalysis &) = delete;
+        CsvRunAnalysis(CsvRunAnalysis &&) = delete;
+        CsvRunAnalysis &operator=(CsvRunAnalysis &&) = delete;
+
+        ///
+        std::map<Scoring::Type, std::map<std::size_t, VoxelHit>> m_hashed_scoring_map;
+
+        ///
+        std::set<Scoring::Type> m_scoring_types = {Scoring::Type::Cell, Scoring::Type::Voxel};
+
+    public:
+        ///
+        static CsvRunAnalysis* GetInstance();
+};
 
 #endif //CSV_RUN_ANALYSIS_HH

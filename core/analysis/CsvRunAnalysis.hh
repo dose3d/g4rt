764432e2e5c02362b3871dbee3ef7a263d2c15bc
Bartek Rachwal <brachwal@agh.edu.pl>
2024-02-13 10:44:18 +0100

    init csvrunanalysis class

---
+++ core/analysis/CsvRunAnalysis.hh
@@ -0,0 +1,21 @@
+//
+// Created by brachwal on 13.02.2024.
+//
+
+#ifndef CSV_RUN_ANALYSIS_HH
+#define CSV_RUN_ANALYSIS_HH
+
+#include "globals.hh"
+
+class CsvRunAnalysis {
+    private:
+
+    public:
+        ///
+        CsvRunAnalysis() = default;
+
+        ///
+        ~CsvRunAnalysis() = default;
+}
+
+#endif //CSV_RUN_ANALYSIS_HH

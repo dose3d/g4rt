fa2fe08e7a18442138ceb2b91501f6df7f2496b6
Bartek Rachwal <brachwal@agh.edu.pl>
2024-02-14 12:11:47 +0100

    clean-up

--- core/analysis/CsvRunAnalysis.hh
+++ core/analysis/CsvRunAnalysis.hh
@@ -6,11 +6,6 @@
 #define CSV_RUN_ANALYSIS_HH
 
 #include "globals.hh"
-#include "Types.hh"
-// #include "VoxelHit.hh"
-// #include "G4Cache.hh"
-
-// typedef std::map<Scoring::Type, std::map<std::size_t, VoxelHit>> ScoringMap;
 
 class CsvRunAnalysis {
     private:
@@ -25,37 +20,10 @@ class CsvRunAnalysis {
         CsvRunAnalysis &operator=(const CsvRunAnalysis &) = delete;
         CsvRunAnalysis(CsvRunAnalysis &&) = delete;
         CsvRunAnalysis &operator=(CsvRunAnalysis &&) = delete;
-        
-        // /// Many HitsCollections can be associated to given collection name 
-        // // (e.g. many sensitive detectors constituting a single detection unit)
-        // static std::map<G4String,std::vector<G4String>> m_run_collection;
-
-        // ///
-        // G4MapCache<G4String,ScoringMap> m_run_scoring_collection;
-
-        // ///
-        // std::set<Scoring::Type> m_scoring_types = {Scoring::Type::Cell, Scoring::Type::Voxel};
-
-        // ///
-        // void FillEventCollection(const G4String& collection_name, const G4Event *evt, VoxelHitsCollection* hitsColl);
-
-        // ///
-        // void FillCellEventCollection(std::map<std::size_t, VoxelHit>& scoring_collection, VoxelHit* hit);
-        // void FillVoxelEventCollection(std::map<std::size_t, VoxelHit>& scoring_collection, VoxelHit* hit);
-
 
     public:
         ///
         static CsvRunAnalysis* GetInstance();
-
-        /// Many HitsCollections can be associated to given run collection
-        // static void AddRunCollection(const G4String& collection_name, const G4String& hc_name);
-
-        ///
-        // void BeginOfRun();
-
-        ///
-        // void EndOfEvent(const G4Event *evt);
 };
 
 #endif //CSV_RUN_ANALYSIS_HH

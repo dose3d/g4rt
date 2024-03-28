ecfec518c818c6d67f1bda9c3a3434392af74bc7
Jakub Hajduga <jhajduga@agh.edu.pl>
2024-03-28 13:48:47 +0100

    TODO def

--- core/geometry/Linac/BeamCollimation/MLC/MlcSimplified.cc
+++ core/geometry/Linac/BeamCollimation/MLC/MlcSimplified.cc
@@ -43,6 +43,29 @@ bool MlcSimplified::IsInField(const G4ThreeVector& vertexPosition) {
     if (m_fieldShape == "RTPlan"){ // TEMP SOLUTION!
         if (abs(vertexPosition.x())<=m_fieldParamA && abs(vertexPosition.y())<= m_fieldParamB)
         return true; 
+
+        /*
+        #include <vector>
+        #include <utility>
+        bool isPointInPolygon(const std::pair<double, double>& point, const std::vector<std::pair<double, double>>& polygon) {
+            bool inside = false;
+            for (int i = 0, j = polygon.size() - 1; i < polygon.size(); j = i++) {
+                if ((polygon[i].second > point.second) != (polygon[j].second > point.second) &&
+                    (point.first < (polygon[j].first - polygon[i].first) * (point.second - polygon[i].second) / (polygon[j].second - polygon[i].second) + polygon[i].first)) {
+                    inside = !inside;
+                }
+            }
+            return inside;
+        }
+
+        TODO:
+        Calc in 2D plane position of corners in MLC.
+        Each corner as a point in polygon.
+        
+
+        */
+
+
     }
     return false;
 }

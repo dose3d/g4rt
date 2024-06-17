00f1e686f75a73bec430fd5baa9affac74b442e4
Jakub Hajduga <jhajduga@agh.edu.pl>
2024-06-17 12:28:28 +0200

    Job Prepared for multiple trays on table

--- core/geometry/WorldConstruction/PatientGeometry.cc
+++ core/geometry/WorldConstruction/PatientGeometry.cc
@@ -199,11 +199,13 @@ void PatientGeometry::Construct(G4VPhysicalVolume *parentPV) {
   m_patient->Construct(pv);
   m_patient->WriteInfo();
 
-  // // Creation of bed?
-  // auto tableMaterial = ConfigSvc::GetInstance()->GetValue<G4MaterialSPtr>("MaterialsSvc", "G4_POLYACRYLONITRILE");
-  // auto tableBox = new G4Box("TableBox", 300.0*mm, 300.0*mm, 5.0*mm);
-  // auto dcoverLV = new G4LogicalVolume(tableBox, tableMaterial.get(), "TableBoxLV");
-  // SetPhysicalVolume(new G4PVPlacement(nullptr, G4ThreeVector(0.0,0.0,60.1), "CoverBoxPV", dcoverLV, parentPV, false, 0));
+  // Creation of bed?
+  auto tableMaterial = ConfigSvc::GetInstance()->GetValue<G4MaterialSPtr>("MaterialsSvc", "G4_POLYACRYLONITRILE");
+  auto tableHeight =  7.0*mm;
+  auto tableBox = new G4Box("TableBox", 1100.0*mm, 225.0*mm, tableHeight);
+  auto dcoverLV = new G4LogicalVolume(tableBox, tableMaterial.get(), "TableBoxLV");
+  SetPhysicalVolume(new G4PVPlacement(nullptr, G4ThreeVector(900.0,0.0,((1.0*mm)+tableHeight+envPosZ+envSize.z())), "CoverBoxPV", dcoverLV, parentPV, false, 0));
+
 
 
 

69e21f4ae933469217450437797792b4ae39f9ff
Jakub Hajduga <jhajduga@agh.edu.pl>
2025-03-05 18:35:41 +0100

    WIP -> Iba update and naming in code.
    TODO!
    Change All Plans (Positioning from CSV move to to D3DDetector_Detector)
    TopPositionInenv rename to TranslationInLocalFrame
    TranslationInLocalFrame moved to Cell
    Uptade fobs to incorporate that.

--- core/geometry/Patient/IbaImRT/IbaImRT.cc
+++ core/geometry/Patient/IbaImRT/IbaImRT.cc
@@ -18,28 +18,36 @@ IbaImRT* IbaImRT::GetInstance() {
 
 ////////////////////////////////////////////////////////////////////////////////
 ///
-G4ThreeVector IbaImRT::IbaToLocalTranslation(-90.0, 165.0, -90.0);
+G4ThreeVector IbaImRT::IbaToLocalTranslation(0.0, 0.0, 0.0);
 ////////////////////////////////////////////////////////////////////////////////
 ///
 void IbaImRT::Construct(G4VPhysicalVolume *parentPV) {
   
-  if (ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop") != "IbaImRT_3mf") {
-
-    auto centreOFPhantomBox = new G4Box("smallCentreOFPhantomBox", 90.0*mm, 90.0*mm, 165.0*mm);
-    auto SideOfPhantomTube = new G4Tubs("SideOfPhantomTube", 0.0*mm, 90.0*mm, 165.0*mm, 0.0*deg, 360.0*deg);
-    auto FirstSideOfPhantom = new G4UnionSolid("SideOfPhantomBox", centreOFPhantomBox, SideOfPhantomTube, nullptr, G4ThreeVector(0.0*mm,-90.0*mm,0.0*mm));
-    auto FullPhantom = new G4UnionSolid("SideOfPhantomBox", FirstSideOfPhantom, SideOfPhantomTube, nullptr, G4ThreeVector(0.0*mm,90.0*mm,0.0*mm));
-    
+  G4VSolid* FullPhantom;
+  auto my_rotation = new G4RotationMatrix;
+  
+  auto boxMaterial = ConfigSvc::GetInstance()->GetValue<G4MaterialSPtr>("MaterialsSvc", "PMMA");
+  if (ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop") == "IbaImRT_Box") {
+    IbaImRT::IbaToLocalTranslation = G4ThreeVector(90.00, -90.00, 90.00);
+    FullPhantom = new G4Box("smallCentreOFPhantomBox", 90.0*mm, 90.0*mm, 90.0*mm);
+  }
+  else if(ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop") == "IbaImRT_Full") {
+      IbaImRT::IbaToLocalTranslation = G4ThreeVector(-90.0, 165.0, -90.0);
+      auto centreOFPhantomBox = new G4Box("smallCentreOFPhantomBox", 90.0*mm, 90.0*mm, 165.0*mm);
+      auto SideOfPhantomTube = new G4Tubs("SideOfPhantomTube", 0.0*mm, 90.0*mm, 165.0*mm, 0.0*deg, 360.0*deg);
+      auto FirstSideOfPhantom = new G4UnionSolid("SideOfPhantomBox", centreOFPhantomBox, SideOfPhantomTube, nullptr, G4ThreeVector(0.0*mm,-90.0*mm,0.0*mm));
+      FullPhantom = new G4UnionSolid("SideOfPhantomBox", FirstSideOfPhantom, SideOfPhantomTube, nullptr, G4ThreeVector(0.0*mm,90.0*mm,0.0*mm));
+      my_rotation->rotateY(90.0*deg);
+      my_rotation->rotateX(90.0*deg);
+    }
+    else {
+      LOGSVC_ERROR("Unknown properties of IbaImRT phantom: {}", ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop"));
+    }
 
     auto FullPhantomLV = new G4LogicalVolume(FullPhantom, boxMaterial.get(), "IbaImRTLV");
     
     // We have to rotate it in order to right alignement of the Geant4 volumes in the world
 
-    // We have to rotate it in order to right alignement of the Geant4 volumes in the world
-    auto my_rotation = new G4RotationMatrix;
-    my_rotation->rotateY(90.0*deg);
-    my_rotation->rotateX(90.0*deg);
-
     // Note: the position is interpreated as an isocentre nad it's injected in real Iba Frame, thus we have to translate to local frame
-    SetPhysicalVolume(new G4PVPlacement(my_rotation, m_position+IbaImRT::IbaToLocalTranslation, "IbaImRTPV", FullPhantomLV, parentPV, false, 0));
+    SetPhysicalVolume(new G4PVPlacement(my_rotation, m_position, "IbaImRTPV", FullPhantomLV, parentPV, false, 0));
 }

ba8e7624351cd15d0933c651ef4ea43ed7938298
Bartek Rachwal <brachwal@agh.edu.pl>
2025-03-06 17:49:40 +0100

    iba positioning - done!

--- core/geometry/Patient/IbaImRT/IbaImRT.cc
+++ core/geometry/Patient/IbaImRT/IbaImRT.cc
@@ -27,11 +27,9 @@ void IbaImRT::Construct(G4VPhysicalVolume *parentPV) {
   
   auto boxMaterial = ConfigSvc::GetInstance()->GetValue<G4MaterialSPtr>("MaterialsSvc", "PMMA");
   if (ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop") == "IbaImRT_Box") {
-    IbaImRT::IbaToLocalTranslation = G4ThreeVector(90.00, -90.00, 90.00);
     FullPhantom = new G4Box("smallCentreOFPhantomBox", 90.0*mm, 90.0*mm, 90.0*mm);
   }
   else if(ConfigSvc::GetInstance()->GetValue<std::string>("PatientGeometry", "EnviromentPatientEnvelop") == "IbaImRT_Full") {
-      IbaImRT::IbaToLocalTranslation = G4ThreeVector(-90.0, 165.0, -90.0);
       auto centreOFPhantomBox = new G4Box("smallCentreOFPhantomBox", 90.0*mm, 90.0*mm, 165.0*mm);
       auto SideOfPhantomTube = new G4Tubs("SideOfPhantomTube", 0.0*mm, 90.0*mm, 165.0*mm, 0.0*deg, 360.0*deg);
       auto FirstSideOfPhantom = new G4UnionSolid("SideOfPhantomBox", centreOFPhantomBox, SideOfPhantomTube, nullptr, G4ThreeVector(0.0*mm,-90.0*mm,0.0*mm));

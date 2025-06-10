40729b9a0ab51a7b1a6695cb73caf8c31f29979c
Bartek Rachwal <brachwal@agh.edu.pl>
2025-06-10 12:06:18 +0200

    touch gtest

--- core/geometry/Patient/PatientTest.hh
+++ core/geometry/Patient/PatientTest.hh
@@ -12,13 +12,14 @@
 class PatientTest : public ::testing::Test {
 protected:
     void SvcSetup(const std::string& toml_file) {
-        auto configSvc = Service<ConfigSvc>();  // initialize ConfigSvc for TOML parsing
-        auto runSvc = Service<RunSvc>();        // get RunSvc for general App run configuration
+        auto configSvc  = Service<ConfigSvc>();  // initialize ConfigSvc for TOML parsing
+        auto runSvc     = Service<RunSvc>();     // get RunSvc for general App run configuration
+        auto geoSvc     = Service<GeoSvc>();
         runSvc->AppMode(OperationalMode::BuildGeometry);
         configSvc->ParseTomlFile(toml_file);
         configSvc->PrintTomlConfig();
-        auto world = Service<GeoSvc>()->World();
-        runSvc->Initialize(world);
+        auto world = geoSvc->World();
+        // runSvc->Initialize(world);
     }
     G4VPhysicalVolume* WorldSetup() {
         auto testWorldSize = G4ThreeVector(200.0*cm,200.0*cm,200.0*cm);

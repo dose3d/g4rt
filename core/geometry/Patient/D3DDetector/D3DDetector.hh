ed95d16a876496e857a4c12b96a8ed9724a5110f
Bartek Rachwał <brachwal@agh.edu.pl>
2025-06-10 09:51:45 +0200

    55 refactor volume calc for the analysis (#60)
    * add volume for vpatient
    
    * add extra cell volume logic
    
    * fix CellInfo ctr issue
    
    * remove ::SIZE usage
    
    * refactor ::SIZE type

--- core/geometry/Patient/D3DDetector/D3DDetector.hh
+++ core/geometry/Patient/D3DDetector/D3DDetector.hh
@@ -59,7 +59,7 @@ class D3DDetector : public VPatient, public GeoComponet {
     std::map<std::size_t, VoxelHit> GetScoringHashedMap(const G4String& scoring_name,Scoring::Type type) const override;
 
     //
-
+    G4double GetCellVolume() const override { return D3DCell::SIZE.getX() * D3DCell::SIZE.getY() * D3DCell::SIZE.getZ(); };
 
     class Config {
       public:

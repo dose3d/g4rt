874482ca1574f3ed977b5403f3a943b6369dc588
Bartek Rachwal <brachwal@agh.edu.pl>
2026-03-24 11:09:12 +0100

    update mlc simplified definition

--- core/geometry/Linac/BeamCollimation/MLC/MlcSimplified.cc
+++ core/geometry/Linac/BeamCollimation/MLC/MlcSimplified.cc
@@ -26,28 +26,68 @@ void MlcSimplified::SetRunConfiguration(const ControlPoint* control_point){
         m_fieldParamB = control_point->GetFieldSizeB();
     } else if (m_fieldShape == "RTPlan" || m_fieldShape == "CustomPlan") {
         INFO_GEO("Initializing MLC from RTPlan");
+        VMlc::m_leaves_x_positioning.clear();
         m_mlc_a_corners.clear();
         m_mlc_b_corners.clear();
         m_mlc_corners.clear();
         const auto& mlc_a_positioning = control_point->GetMlcPositioning("Y1");
-        double x_half_width = 2.5/2; // mm
-        double x_init = 30 * x_half_width * 2 - x_half_width; // mm
-        VMlc::m_leaves_x_positioning.clear();
-        for(int leaf_idx = 0; leaf_idx < mlc_a_positioning.size(); leaf_idx++){
-            double leaf_a_pos_y = mlc_a_positioning.at(leaf_idx);
-            double leaf_a_pos_x = - x_init + leaf_idx * 2.5; // TEMP! fixed to 2.5 mm, TODO: getLeafAPosition(leaf_idx);
-            m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_a_pos_x - x_half_width);
-            m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_a_pos_x + x_half_width);
-
-            VMlc::m_leaves_x_positioning.push_back(leaf_a_pos_x);
-        }
         const auto& mlc_b_positioning = control_point->GetMlcPositioning("Y2");
-        for(int leaf_idx = 0; leaf_idx < mlc_b_positioning.size(); leaf_idx++){
+        double inner_leaf_width = 2.5; // mm
+        double outer_leaf_width = 5.0; // mm
+        int total_leaves = 60;
+        int inner_leaf_count = 32;
+        int outer_each_side = (total_leaves - inner_leaf_count) / 2;
+        double total_width = 220.0;
+        double current_x = - total_width / 2.0;
+
+        for(int leaf_idx = 0; leaf_idx < mlc_a_positioning.size(); leaf_idx++) {
+            bool isInner = (leaf_idx >= outer_each_side && 
+                leaf_idx < outer_each_side + inner_leaf_count);
+            double leaf_width = isInner ? inner_leaf_width : outer_leaf_width;
+            double x_half_width = leaf_width / 2.0;
+            double leaf_center_x = current_x + x_half_width;
+            double leaf_a_pos_y = mlc_a_positioning.at(leaf_idx);
             double leaf_b_pos_y = mlc_b_positioning.at(leaf_idx);
-            double leaf_b_pos_x = - x_init + leaf_idx * 2.5; // TEMP! fixed to 2.5 mm, TODO: getLeafBPosition(leaf_idx);
-            m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_b_pos_x - x_half_width);
-            m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_b_pos_x + x_half_width);
+
+            m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_center_x - x_half_width);
+            m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_center_x + x_half_width);
+            m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_center_x - x_half_width);
+            m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_center_x + x_half_width);
+
+            VMlc::m_leaves_x_positioning.push_back(leaf_center_x);
+            current_x += leaf_width;
         }
+        // double x_half_width = 2.5/2; // mm
+        // double x_init = 30 * x_half_width * 2 - x_half_width; // mm
+        // for(int leaf_idx = 0; leaf_idx < mlc_a_positioning.size(); leaf_idx++){
+        //     double leaf_a_pos_y = mlc_a_positioning.at(leaf_idx);
+        //     double leaf_a_pos_x = - x_init + leaf_idx * 2.5; // TEMP! fixed to 2.5 mm, TODO: getLeafAPosition(leaf_idx);
+        //     m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_a_pos_x - x_half_width);
+        //     m_mlc_a_corners.emplace_back(leaf_a_pos_y, leaf_a_pos_x + x_half_width);
+
+        //     VMlc::m_leaves_x_positioning.push_back(leaf_a_pos_x);
+        // }
+        // const auto& mlc_b_positioning = control_point->GetMlcPositioning("Y2");
+        // for(int leaf_idx = 0; leaf_idx < mlc_b_positioning.size(); leaf_idx++){
+        //     double leaf_b_pos_y = mlc_b_positioning.at(leaf_idx);
+        //     double leaf_b_pos_x = - x_init + leaf_idx * 2.5; // TEMP! fixed to 2.5 mm, TODO: getLeafBPosition(leaf_idx);
+        //     m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_b_pos_x - x_half_width);
+        //     m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_b_pos_x + x_half_width);
+        // }
+        // current_x = - total_width / 2.0;
+        // for(int leaf_idx = 0; leaf_idx < mlc_b_positioning.size(); leaf_idx++){
+        //     bool isInner = (leaf_idx >= outer_each_side && 
+        //         leaf_idx < outer_each_side + inner_leaf_count);
+        //     double leaf_width = isInner ? inner_leaf_width : outer_leaf_width;
+        //     double x_half_width = leaf_width / 2.0;
+        //     double leaf_center_x = current_x + x_half_width;
+        //     double leaf_b_pos_y = mlc_b_positioning.at(leaf_idx);
+        //     m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_center_x - x_half_width);
+        //     m_mlc_b_corners.emplace_back(leaf_b_pos_y, leaf_center_x + x_half_width);
+        //     current_x += leaf_width;
+        // }
+
+
         std::reverse(m_mlc_b_corners.begin(), m_mlc_b_corners.end());
         m_mlc_corners.insert(m_mlc_corners.end(), m_mlc_a_corners.begin(), m_mlc_a_corners.end());
         m_mlc_corners.insert(m_mlc_corners.end(), m_mlc_b_corners.begin(), m_mlc_b_corners.end());
